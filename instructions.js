9140163449bb7f1e1e27606d3e84552dc85f24de
Landon Vago-Hughes <landon2008@hotmail.com>
2017-11-10 02:25:40 +0000

    more functionality

--- instructions.js
+++ instructions.js
@@ -8,10 +8,19 @@ class Instructions {
         this.commandKeys = {
             enter: 13,
         };
-        this.command = {}
         this.directory = "/home"
+        this.directoryLevel = 0
         this.possiblepaths = ['projects', 'contact']
 
+        this.directorytotal = {
+
+            0: ['projects', 'contact'],
+            1: ["email: landonvagohughes@gmail.com", "github: lvh1g15", "linkedIn: linkedin.com/in/landon-vago-hughes-01a47712a"],
+            2: ["https://cocoapods.org/?q=sliderprogress", "https://github.com/lvh1g15/SearchBar-Animation"]
+
+        }
+
+        this.possiblecommands = ['cd: Travel into directory', 'ls: Show contents of directory', 'Help: List commands', 'open: Open content', 'pwd: View your current location']
         this.contact = ["email: landonvagohughes@gmail.com", "github: lvh1g15", "linkedIn: linkedin.com/in/landon-vago-hughes-01a47712a"]
         this.projects = ["https://cocoapods.org/?q=sliderprogress", "https://github.com/lvh1g15/SearchBar-Animation"]
         Instructions.init()
@@ -36,17 +45,34 @@ class Instructions {
     }
 
     handlecommand(cmd, args) {
-        console.log(cmd)
+
         if(cmd == 'pwd'){
             return this.directory
         } else if(cmd == "ls"){
-            console.log(this.possiblepaths)
-            return this.listdirectories(this.possiblepaths)
+
+            if(this.directoryLevel == 0) {
+                return this.listdirectories(this.directorytotal[0])
+            }else if(this.directoryLevel == 1) {
+                return this.listdirectories(this.directorytotal[1])
+            }else if(this.directoryLevel == 2) {
+                return this.listdirectories(this.directorytotal[2])
+            }
+
         } else if(cmd == "cd"){
             if(args == "contact"){
+
+                if(this.directorytotal != 1){
+
+                }
+
                 this.directory += "/contact"
-                return this.listdirectories(this.contact)
+                this.directoryLevel = 1
+            }else if(args == 'projects'){
+                this.directory += "/projects"
+                this.directoryLevel = 2
             }
+        } else if(cmd == "Help"){
+            return this.listdirectories(this.possiblecommands)
         }
     }
 
@@ -70,16 +96,16 @@ class Instructions {
 
                 if(command == 'clear') {
                     this.clearhistory()
-                } else if(command && ['pwd', 'ls', 'open', 'cd'].includes(command)) {
-                    this.instructions.innerHTML += this.handlecommand(command, args)
+                } else if(command && ['pwd', 'ls', 'open', 'cd', 'Help'].includes(command)) {
+                    if(command === 'cd'){
+                        this.handlecommand(command, args)
+                    }else{
+                        this.instructions.innerHTML += this.handlecommand(command, args)
+                    }
                     this.reset(target)
                 } else {
                     this.instructions.innerHTML += "<p style='padding-left:  10px; font-family: Menlo; font-size: 15px; color: lime'>Error: command not recognized</p>";
                     this.reset(target);
-                }
-
-                if(command.value == "Help"){
-                    alert("You have proceeded to help");
                 }
                 event.preventDefault()
             }
@@ -100,4 +126,22 @@ class Instructions {
         copy.querySelector('#inputcommands').innerHTML = '';
         copy.querySelector('#inputcommands').focus();
     }
+
+}
+
+
+
+class errors {
+
+    constructor() {
+        this.cderror = ''
+        this.invalidcmd = ''
+        this.init()
+    }
+
+    init(){
+
+    }
+
+    
 }
