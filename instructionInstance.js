cc5b8e4cf96cd7d994403fb65c0b405bffd7eeca
LandonYSplit <landonvagohughes3@gmail.com>
2025-02-26 13:01:26 -0500

    remove old shit

--- instructionInstance.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-
-    const instructions = document.getElementById('instructions');
-    const terminal = new Instructions(instructions);
-
-});
